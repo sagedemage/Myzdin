f553a041693706b909abc143ff2a7c09677e8994
Salmaan Saeed <salsaeed999@gmail.com>
2023-06-16 01:22:10 -0400

    deleted extra source files

--- src/ground/ground.c
+++
@@ -1,6 +0,0 @@
-#include "ground.h"
-
-void RenderGround(SDL_Renderer* rend, struct Ground ground) {
-    /* Render the ground */
-    SDL_RenderCopy(rend, ground.GroundTex, &ground.srcrect, &ground.dstrect);
-}
