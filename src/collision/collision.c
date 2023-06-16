8336a42224e1c2330c586b258b5cbb2bb6ad3fb1
Salmaan Saeed <salsaeed999@gmail.com>
2023-06-16 01:49:49 -0400

    removed collision source file

--- src/collision/collision.c
+++
@@ -1,9 +0,0 @@
-#include "../level.h"
-#include "../player/player.h"
-
-void collision_soundeffect(Mix_Chunk* landing_noise) {
-    // Collison soundeffect
-    Mix_PlayChannel(1, landing_noise, 0); // acceleration not changing quick enough
-}
-
-
--- src/collision/collision.h
+++
@@ -1,6 +0,0 @@
-#ifndef COLLISION_H
-#define COLLISION_H
-
-void collision_soundeffect(Mix_Chunk* landing_noise);
-
-#endif // COLLISION_H
